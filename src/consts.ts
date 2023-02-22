67a56e89d9e0d66df3c412b1d51179f3e9066164
Kananek Thongkam <kananekt@scg.com>
2023-02-22 18:44:34 +0700

    DAI-00/chore: update style

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'WE-DO';
+export const SITE_TITLE = 'WEDO';
 export const SITE_DESCRIPTION = 'WEDO Stay humble, pursue your passion, be fearless and create a purposeful innovation for a better future with us.';
